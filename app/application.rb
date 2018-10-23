3a520ae66f924b813a36aac6c37d0779cf067c88
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 09:28:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application 
   def call(env)
     resp = Rack::Response.new 
-    if Time.now.hour 
+    if Time.now.hour => 00
       resp.write "Good morning"
     else
       resp.write"Good afternoon"
