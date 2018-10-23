f0a55f53c0090bba1c64e86e70ccbae76c75ae10
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 09:22:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,12 @@
 class Application 
+  def call(env)
+    resp = Rack::Response.new 
+    if timenow 
+      resp.write "Goodmorning"
+    else
+      resp.write"Good afternoon"
+    end 
+  resp.finish
+  end 
+end 
   
