67e618a2f77f6eb9e93c9f335bade2af8129ac89
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-23 09:22:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     if timenow 
-      resp.write "Goodmorning"
+      resp.write "Good morning"
     else
       resp.write"Good afternoon"
     end 
